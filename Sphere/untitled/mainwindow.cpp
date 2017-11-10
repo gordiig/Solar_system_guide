39e2f84bb55c12fbcc513eef5f3569b9c3d9aa8d
Gordiig <gordiig@gmail.com>
2017-11-10 20:25:51 +0300

    Composing

--- Sphere/untitled/mainwindow.cpp
+++ Sphere/untitled/mainwindow.cpp
@@ -50,6 +50,7 @@ void MainWindow::on_ButOpen_clicked()
     {
         std::string dir = "/Users/gordiig/Desktop/Cur_Sem/Un_CourseProject_Graph/Sphere/Contents/";
         dir += ui->LineEdit->text().toStdString();
+        //dir += "new_med.obj";
         in_dot.read(dir.c_str());
         in_dot.draw(gr);
         scene->clear();
